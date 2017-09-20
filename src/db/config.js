ed17a10254ec4ac5b44ebc279cd4b5b860cb1f88
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-09-20 13:34:46 -0700

    Add default connection database url

--- src/db/config.js
+++ src/db/config.js
@@ -2,8 +2,8 @@ module.exports = (env) => {
   if (env === 'development') {
     return process.env.DB_DEV_URL
   } else if (env === 'test') {
-    return process.env.DB_TEST_URL
+    return process.env.DB_TEST_URL || 'postgres://localhost:5432/contacts_test'
   } else {
-    return process.env.DATABASE_URL
+    return process.env.DATABASE_URL || 'postgres://localhost:5432/contacts_development'
   }
 }
